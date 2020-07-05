7ff6692610c5f6bd298e269109e1e0819b6e5127
ischm001 <ischm001@scm.mi.hs-rm.de>
2020-07-05 16:32:42 +0200

    Bilder geändert

--- HamburgGradle/src/main/java/business/components/Vehicle.java
+++ HamburgGradle/src/main/java/business/components/Vehicle.java
@@ -1,35 +1,47 @@
 package business.components;
 
+import javafx.animation.AnimationTimer;
+
 public class Vehicle {
 
 	int positionX;
 	int positionY;
 	int alignment;
+
 	double width;
 	double length;
-//	String color;
 	
-//	public Vehicle() {
-//		
-//	}
-//	
-//	private void placeUp() {
-//		
-//	}
-//	
-//	private void placeDown() {
-//		
-//	}
-//	
-//	private void placeLeft() {
-//		
-//	}
-//	
-//	private void placeRight() {
-//		
-//	}
+	boolean running;
+	
+	String color;
+	
+	AnimationTimer timer;
+	
+	/*fahren-methode muss hier rein womit wir die autos auf den straﬂen fahren lassen 
+	 * 
+	 */
+	
+	private class MyTimer extends AnimationTimer {
+
+		@Override
+		public void handle(long now) {
+			start();
+			
+		}
+		
+	}
+	
+	public Vehicle() {		
+	}
 	
 	private void remove() {
 		
+	}
+	
+	private void drive() {
+		
+		while(running = true) {
+			
+		}
 	}
 }
